a5a055b02f5ab67cfd931a91838c8d891cd5e3a7
Bruno Gomes de Souza <124291749+brunogsouza10@users.noreply.github.com>
2024-03-12 13:16:15 -0300

    Delete matricula directory

--- matricula/src/main/java/com/example/matricula/matricula/repository/MatriculaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.matricula.matricula.repository;
-
-import com.example.matricula.matricula.dto.Matricula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
-}
